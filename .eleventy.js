599dac5e9be748d9c9025b46859568dbf7b3fa4d
Dan Ott <thedanielott@gmail.com>
2020-12-18 14:02:46 -0500

    📙 Add support for definition lists to Markdown
    I'm beginning to author a page where I want an HTML definition list.
    This is possible without extraneous typing via a [Markdown extension][].
    
    Following the [sample configuration][] from the Eleventy guides, I've
    added this feature to the `MarkdownIt` parser used by this site.
    
    [markdown extension]: https://www.markdownguide.org/extended-syntax#definition-lists
    [sample configuration]: https://www.11ty.dev/docs/languages/markdown/#add-your-own-plugins

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,16 @@
+const MarkdownIt = require("markdown-it")
+
+const md = new MarkdownIt({
+  html: true,
+  breaks: false,
+  typographer: true,
+}).use(require("markdown-it-deflist"))
+
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy("./site/site.css")
   eleventyConfig.addWatchTarget("./site/site.css")
+
+  eleventyConfig.setLibrary("md", md)
 
   return {
     htmlTemplateEngine: "njk",
